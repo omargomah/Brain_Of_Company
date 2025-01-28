bfa626ec15e75497950042d0a429f0271bb7b18e
sherif <sherifelgendy2004@gmail.com>
2025-01-28 16:30:07 +0200

    invoices, Dependant Done

---
+++ Brain_Of_Company/Brain_Of_Company_Solution/Brain_DAL/Data/configure/DependentConfigure.cs
@@ -0,0 +1,21 @@
+﻿using Brain_Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brain_DAL.Data.configure
+{
+    internal class DependentConfigure : IEntityTypeConfiguration<Dependent>
+    {
+        public void Configure(EntityTypeBuilder<Dependent> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).HasColumnName("Id").HasColumnType("int").IsRequired(true);
+            builder.Property(x => x.Name).HasColumnName("Name").HasColumnType("varchar").HasMaxLength(100).IsRequired(true);
+        }
+    }
+}
---
+++ Brain_Of_Company/Brain_Of_Company_Solution/Brain_DAL/Data/configure/InvoiceConfigure.cs
@@ -0,0 +1,23 @@
+﻿using Brain_Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brain_DAL.Data.configure
+{
+    internal class InvoiceConfigure : IEntityTypeConfiguration<Invoice>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Invoice> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).HasColumnName("Id").HasColumnType("int").IsRequired(true);
+            builder.Property(x => x.DOS).HasColumnName("DateOfSale").HasColumnType("DateTime").IsRequired(true);
+            builder.Property(x => x.IsDeleted).HasColumnName("Isdeleted").HasColumnType("BIT").IsRequired(true);
+            builder.Property(x => x.Quantities).HasColumnName("Quantities").HasColumnType("int").IsRequired(true);
+            builder.Property(x => x.Price).HasColumnName("Price").HasColumnType("Decimal").IsRequired(true);
+        }
+    }
+}
