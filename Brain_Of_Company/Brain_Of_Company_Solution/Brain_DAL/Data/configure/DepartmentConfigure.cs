3e3e7a5706aba531ab2215e429b95d8fe2ef9399
Omar Gomaa <mrjmh934@gmail.com>
2025-01-28 16:51:53 +0200

    add the configure of Department

---
+++ Brain_Of_Company/Brain_Of_Company_Solution/Brain_DAL/Data/configure/DepartmentConfigure.cs
@@ -0,0 +1,20 @@
+﻿using Brain_Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Brain_DAL.Data.configure
+{
+    internal class DepartmentConfigure : IEntityTypeConfiguration<Department>
+        {
+            public void Configure(EntityTypeBuilder<Department> builder)
+            {
+                builder.HasKey(x => x.Id);
+                builder.Property(x => x.Id).HasColumnName("Id").HasColumnType("int").IsRequired(true);
+                builder.Property(x => x.Name).HasColumnName("Name").HasColumnType("varchar").IsRequired(true);
+                builder.Property(x => x.MinimumDaysToAttendancePerMonth).HasColumnName("MinimumDaysToAttendancePerMonth").HasColumnType("int").IsRequired(true);
+                builder.Property(x => x.ManagerSSN).HasColumnName("ManagerSSN").HasColumnType("int").IsRequired(true);
+                builder.Property(x => x.Location).HasColumnName("Location").HasColumnType("varchar").IsRequired(true);
+            }
+        }
+
+}
